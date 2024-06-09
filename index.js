7c87cf95f248751a371b0471e7727baeccd08454
Santi Mera <santigomero@gmail.com>
2024-06-09 09:49:20 +0300

    FT_RMT_27052024 Santiago

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const string1 = "My favorite dessert is jello";
 
 // Your code here...
 
-
+string1.indexOf("j");
 
 
 /*******************************************
@@ -19,7 +19,7 @@ const string2 = "ABCDEFGHJKLO";
 
 // Your code here...
 
-
+console.log(string2[2] + string2[11] + string2[11] + string2[10]);
 
 
 /*****************************************************
@@ -31,7 +31,7 @@ const string3 = "Na";
 
 // Your code here...
 
-
+console.log(string3.repeat(4) + " Batman!");
 
 
 /*******************************************
@@ -42,6 +42,8 @@ const string3 = "Na";
 const fruit = "banana apple mango orange lemon kiwi watermelon grapes pear pineapple";
 
 // Your code here...
+
+console.log(fruit.slice(13, 18));
 
 
 
@@ -58,11 +60,20 @@ const funnyHeadline2 = "Students Cook & Serve Grandparents";
 
 // Check the first headline
 // Your code here ...
-
+if (funnyHeadline1.includes("oxygen")){
+    console.log("The includes return the word oxygen")
+} else{
+    console.log("The string does not include the word oxygen")
+}
 
 // Check the second headline
 // Your code here ...
 
+if (funnyHeadline2.includes("oxygen")){
+    console.log("The string return the word oxygen")
+} else{
+    console.log("The string does not include the word oxygen")
+}
 
 
 /*******************************************
@@ -76,6 +87,7 @@ const string4 = "zEAWrTC9EgtxmK9w1";
 // a) Print the string length
 // Your code here ...
 
-
+console.log(string4.length)
 // b) Print the last character in the string
 // Your code here ...
+console.log(string4[16]);
